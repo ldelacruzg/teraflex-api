b78c1826ab859e5b00e882ae1b3b12b3c688ff75
Mauricio Manzaba <mauricio.9.inm@gmail.com>
2023-06-24 15:03:51 -0500

    configuración inicial

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Teraflex API')
+    .setDescription('Documentación de la API de Teraflex')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors({
+    origin: '*',
+  });
+
+  await app.listen(3000);
+}
+
+bootstrap();
