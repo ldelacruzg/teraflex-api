bb2658569851f2ff9a421dfd587bad8c62105a9b
Luis De La Cruz <ldelacruzg@uteq.edu.ec>
2023-07-13 18:28:37 -0500

    fix: changes the data returned on getting the assigned tasks

--- src/activity/controllers/assignment/dto/create-assigment.dto.ts
+++ src/activity/controllers/assignment/dto/create-assigment.dto.ts
@@ -1,5 +1,6 @@
 import { CreateManyAssignmentsDto } from './create-many-assignments.dto';
 
-export type CreateAssignmentDto = Omit<CreateManyAssignmentsDto, 'taskIds'> & {
+export type CreateAssignmentDto = Omit<CreateManyAssignmentsDto, 'tasks'> & {
+  userId: number;
   taskId: number;
 };
