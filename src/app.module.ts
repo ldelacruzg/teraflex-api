e5ba4c99e5a71bb3a08970051176221afb6a3fec
Mauricio Manzaba <mauricio.9.inm@gmail.com>
2023-09-18 18:13:53 -0500

    feature: módulo de caché

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,6 +20,8 @@ import { MultimediaModule } from '@multimedia/multimedia.module';
 import { NotificationModule } from '@/notifications/notification.module';
 import { Notification } from '@entities/notification.entity';
 import { NotificationToken } from '@entities/notification-token.entity';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -55,8 +57,15 @@ import { NotificationToken } from '@entities/notification-token.entity';
     ActivityModule,
     MultimediaModule,
     NotificationModule,
+    CacheModule.register({ isGlobal: true }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
