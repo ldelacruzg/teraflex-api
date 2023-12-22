9c3edff34575ef64d7c9a6fceb85e34745c3b601
Luis De La Cruz <ldelacruzg@uteq.edu.ec>
2023-12-22 17:04:25 -0500

    fix: removes current_rank field

--- src/gamification/patient-leaderboards/domain/patient-leaderboard.entity.ts
+++ src/gamification/patient-leaderboards/domain/patient-leaderboard.entity.ts
@@ -7,7 +7,7 @@ import {
   PrimaryGeneratedColumn,
   Relation,
 } from 'typeorm';
-import { Rank } from '@/gamification/leaderboards/domain/rank.enum';
+
 import { Leaderboard, Patient } from '@/entities';
 
 @Entity({ name: 'patient_leaderboards' })
@@ -20,14 +20,6 @@ export class PatientLeaderboard {
 
   @CreateDateColumn({ name: 'joining_date', type: 'timestamp with time zone' })
   joiningDate: Date;
-
-  @Column({
-    name: 'current_rank',
-    nullable: false,
-    type: 'character varying',
-    length: 25,
-  })
-  currentRank: Rank;
 
   @ManyToOne(() => Patient, (patient) => patient.patientLeaderboards)
   @JoinColumn({ name: 'patient_id' })
