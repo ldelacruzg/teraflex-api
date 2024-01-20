21717dc537608b6fa9ab2f22f0cc7d5020118879
Luis De La Cruz <ldelacruzg@uteq.edu.ec>
2024-01-20 17:14:31 -0500

    feat: adds verification of a leaderboard with the patient's rank

--- src/gamification/leaderboards/domain/leaderboard.repository.ts
+++ src/gamification/leaderboards/domain/leaderboard.repository.ts
@@ -5,4 +5,7 @@ import { CreateLeaderboardDto } from './dtos/create-learderboard.dto';
 export abstract class LeaderboardRepository extends Resource<
   Leaderboard,
   CreateLeaderboardDto
-> {}
+> {
+  // verifica la existencia de una tabla de clasificación con el rango del paciente
+  abstract patientRankLeaderboardExists(patientId: number): Promise<boolean>;
+}
--- src/gamification/leaderboards/infrastructure/leaderboard-typeorm-postgres.repository.ts
+++ src/gamification/leaderboards/infrastructure/leaderboard-typeorm-postgres.repository.ts
@@ -2,7 +2,10 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { CreateLeaderboardDto } from '../domain/dtos/create-learderboard.dto';
 import { Leaderboard } from '../domain/leaderboard.entity';
 import { LeaderboardRepository } from '../domain/leaderboard.repository';
-import { Repository } from 'typeorm';
+import { EntityManager, Repository } from 'typeorm';
+import { Inject } from '@nestjs/common';
+import { Patient } from '@/entities';
+import { FormatDateService } from '@/shared/services/format-date.service';
 
 export class LeaderboardRepositoryTypeOrmPostgres
   implements LeaderboardRepository
@@ -10,7 +13,21 @@ export class LeaderboardRepositoryTypeOrmPostgres
   constructor(
     @InjectRepository(Leaderboard)
     private readonly repository: Repository<Leaderboard>,
+    @Inject(EntityManager) private readonly entityManager: EntityManager,
   ) {}
+  patientRankLeaderboardExists(patientId: number): Promise<boolean> {
+    const rangeDate = FormatDateService.getCurrentDateRange();
+
+    return this.entityManager.transaction(async (tx) => {
+      const patient = await tx.findOne(Patient, { where: { id: patientId } });
+      return await tx
+        .createQueryBuilder(Leaderboard, 'l')
+        .where('l.rank = :rank', { rank: patient.rank })
+        .andWhere('l.startDate = :startDate', { startDate: rangeDate.start })
+        .andWhere('l.endDate = :endDate', { endDate: rangeDate.end })
+        .getExists();
+    });
+  }
 
   create(payload: CreateLeaderboardDto): Promise<Leaderboard> {
     throw new Error('Method not implemented.');
