bb2e6aa9b6e02c9406ce3bac161a810e5090dded
Luis De La Cruz <ldelacruzg@uteq.edu.ec>
2024-01-29 01:11:30 -0500

    feat: get current week leaderboard by patient

--- src/gamification/leaderboards/domain/leaderboard-service.interface.ts
+++ src/gamification/leaderboards/domain/leaderboard-service.interface.ts
@@ -1,6 +1,12 @@
 import { Resource } from '@/shared/interfaces/resource.interface';
 import { Leaderboard } from './leaderboard.entity';
 import { CreateLeaderboardDto } from './dtos/create-learderboard.dto';
+import { CurrentWeekLeaderboardRow } from './dtos/current-leaderboard';
 
 export interface ILeaderboardService
-  extends Resource<Leaderboard, CreateLeaderboardDto> {}
+  extends Resource<Leaderboard, CreateLeaderboardDto> {
+  // obtener la tabla de clasificación actual por paciente
+  getCurrentWeekLeaderboardByPatient(
+    patientId: number,
+  ): Promise<CurrentWeekLeaderboardRow[]>;
+}
--- src/gamification/leaderboards/infrastructure/leaderboard.service.ts
+++ src/gamification/leaderboards/infrastructure/leaderboard.service.ts
@@ -1,12 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { ILeaderboardService } from '../domain/leaderboard-service.interface';
 import { CreateLeaderboardDto } from '../domain/dtos/create-learderboard.dto';
 import { Leaderboard } from '../domain/leaderboard.entity';
 import { LeaderboardRepository } from '../domain/leaderboard.repository';
+import { LeaderboardMapper } from './mappers/leaderboard.mapper';
+import { CurrentWeekLeaderboardRow } from '../domain/dtos/current-leaderboard';
 
 @Injectable()
 export class LeaderboardService implements ILeaderboardService {
   constructor(private readonly repository: LeaderboardRepository) {}
+  async getCurrentWeekLeaderboardByPatient(
+    patientId: number,
+  ): Promise<CurrentWeekLeaderboardRow[]> {
+    // verificar si el paciente pertece a la tabla de clasificación actual
+    const pLeaderboard =
+      await this.repository.findCurrentWeekPatientLeaderboard(patientId);
+
+    if (!pLeaderboard) {
+      throw new NotFoundException(
+        'El paciente aún no pertenece a una tabla de clasificación',
+      );
+    }
+
+    // obtener el rango del paciente
+    const { rank } = await this.repository.findOne(pLeaderboard.leaderboardId);
+
+    // obtener la tabla de clasificación actual
+    const leaderboard = await this.repository.findCurrentWeekLeaderboard(rank);
+    return LeaderboardMapper.toCurrentLeaderboardRaw(leaderboard);
+  }
 
   create(payload: CreateLeaderboardDto): Promise<Leaderboard> {
     throw new Error('Method not implemented.');
