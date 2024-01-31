e7c1853cba2fa75e8e91c6a5c808831f02fea0b1
Luis De La Cruz <ldelacruzg@uteq.edu.ec>
2024-01-31 00:21:34 -0500

    feat: get global summary service

--- src/gamification/patients/domain/patient-service.interface.ts
+++ src/gamification/patients/domain/patient-service.interface.ts
@@ -1,5 +1,8 @@
 import { Resource } from '@/shared/interfaces/resource.interface';
 import { Patient } from './patient.entity';
 import { CreatePatientDto } from './dtos/create-patient.dto';
+import { GlobalSummaryDto } from './dtos/global-summary.dto';
 
-export interface IPatientService extends Resource<Patient, CreatePatientDto> {}
+export interface IPatientService extends Resource<Patient, CreatePatientDto> {
+  getGlobalSummary(patientId: number): Promise<GlobalSummaryDto>;
+}
--- src/gamification/patients/infrastructure/patient.service.ts
+++ src/gamification/patients/infrastructure/patient.service.ts
@@ -1,13 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 
 import { Patient } from '../domain/patient.entity';
 import { IPatientService } from '../domain/patient-service.interface';
 import { PatientRepository } from '../domain/patient.repository';
 import { CreatePatientDto } from '../domain/dtos/create-patient.dto';
+import { TreatmentTaskRepository } from '@/activities/treatment-tasks';
+import { GlobalSummaryDto } from '../domain/dtos/global-summary.dto';
 
 @Injectable()
 export class PatientService implements IPatientService {
-  constructor(private readonly repository: PatientRepository) {}
+  constructor(
+    private readonly repository: PatientRepository,
+    private readonly tretamentTaskRepository: TreatmentTaskRepository,
+  ) {}
+
+  async getGlobalSummary(patientId: number): Promise<GlobalSummaryDto> {
+    // verificar si existe el usuario
+    const patient = await this.findOne(patientId);
+
+    if (!patient) {
+      throw new BadRequestException(
+        `El paciente con id (${patientId}) no existe`,
+      );
+    }
+
+    const { flexicoins, experience, rank } = patient;
+    const [weekly, history] = await Promise.all([
+      this.tretamentTaskRepository.getWeeklyAssignedAndCompletedTasks(
+        patientId,
+      ),
+      this.tretamentTaskRepository.getAssignedAndCompletedTasksHistory(
+        patientId,
+      ),
+    ]);
+
+    const globalSummary: GlobalSummaryDto = {
+      flexicoins,
+      experience,
+      rank,
+      qtyTasksHistory: history.qty_tasks,
+      qtyTasksCompletedHistory: history.qty_tasks_completed,
+      qtyTasksWeekly: weekly.qty_tasks,
+      qtyTasksCompletedWeekly: weekly.qty_tasks_completed,
+    };
+
+    return globalSummary;
+  }
 
   create(payload: CreatePatientDto): Promise<Patient> {
     return this.repository.create(payload);
@@ -18,7 +56,7 @@ export class PatientService implements IPatientService {
   }
 
   findOne(id: number): Promise<Patient> {
-    throw new Error('Method not implemented.');
+    return this.repository.findOne(id);
   }
 
   update(id: number, payload: CreatePatientDto): Promise<Patient> {
